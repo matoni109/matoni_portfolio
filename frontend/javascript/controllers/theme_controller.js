4f3f64063d4d004c0c45920fa4fb1c4b9892f1c6
Chris Wade <chris@wadespace.net>
2021-11-14 16:26:42 +1100

    update

--- frontend/javascript/controllers/theme_controller.js
+++ frontend/javascript/controllers/theme_controller.js
@@ -5,13 +5,37 @@ export default class extends Controller {
 
   connect() {
     console.log("Hello, Theme Controller is ON!", this.element);
+    const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+    if (prefersDarkScheme.matches) {
+      this.class.classList.add("sl-theme-dark");
+    } else {
+      this.class.classList.remove("sl-theme-dark");
+    }
+
+    const currentTheme = localStorage.getItem("theme");
+    if (currentTheme == "dark") {
+      let theme = "dark";
+      this.class.classList.toggle("sl-theme-dark");
+      localStorage.setItem("theme", theme);
+      console.log(localStorage.getItem("theme"));
+    }
   }
 
   switch() {
-    // const element = this.nameTarget;
-    // const name = element.value;
-    console.log(`${this.class} is the class`);
-    this.class.classList.toggle("sl-theme-dark");
+    const currentTheme = localStorage.getItem("theme");
+    console.log(currentTheme);
+    if (currentTheme == "dark") {
+      let theme = "light";
+      this.class.classList.toggle("sl-theme-dark");
+      localStorage.setItem("theme", theme);
+      console.log(localStorage.getItem("theme"));
+    } else {
+      let theme = "dark";
+      this.class.classList.toggle("sl-theme-dark");
+      localStorage.setItem("theme", theme);
+      console.log(localStorage.getItem("theme"));
+    }
   }
 
   get class() {
