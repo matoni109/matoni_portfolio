10e3d2e13781f3193b1e5a882ca70d9c21c02783
Chris Wade <chris.wade@discolabs.com>
2022-05-10 19:58:45 +1000

    Update tests

--- test/test_homepage.rb
+++ test/test_homepage.rb
@@ -17,13 +17,13 @@ class TestBlog < Minitest::Test
   context 'blog page' do
     setup do
       page = site.generated_pages.find { |page| page.data.slug == 'posts' }
-      document_root page
+      pp document_root page
     end
 
     should 'show posts list' do
       assert_select 'ul' do |elements|
         elements.each do |element|
-          assert_select element, 'li', 5
+          assert_select element, 'li', 3
         end
       end
     end
